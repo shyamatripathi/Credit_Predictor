21f42b51320a6a2969ceaf12c4dbf22c51d8a3b1
Shyama Tripathi <115423865+shyamatripathi@users.noreply.github.com>
2025-04-15 15:19:16 +0530

    Update urls.py

--- predictor/urls.py
+++ predictor/urls.py
@@ -1,16 +1,28 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from django.urls import path
+# from predictor import views
+# from .views import predict_credit_score_api
+# app_name = 'predictor'  # ✅ Required for URL namespacing
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     # path('', views.home, name='home')
+#     path('', views.home, name='home'),
+#     path('download_pdf/', views.download_pdf, name='download_pdf'),
+#     path('predict/', views.predict_credit_score, name='predict_credit_score'),
+#     path('api/predict/', predict_credit_score_api, name='predict_credit_score_api'),
+# ]
+
 from django.urls import path
-from predictor import views
+from . import views
 from .views import predict_credit_score_api
-app_name = 'predictor'  # ✅ Required for URL namespacing
+
+app_name = 'predictor'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('', views.home, name='home')
     path('', views.home, name='home'),
     path('download_pdf/', views.download_pdf, name='download_pdf'),
     path('predict/', views.predict_credit_score, name='predict_credit_score'),
     path('api/predict/', predict_credit_score_api, name='predict_credit_score_api'),
 ]
-
 
