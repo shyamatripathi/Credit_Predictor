ab3ff2300cae4fafe451e9f6b29c53001818d67e
Shyama Tripathi <115423865+shyamatripathi@users.noreply.github.com>
2025-04-16 14:59:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
+set -e  # Exit immediately if any command fails
+
 echo "Installing gdown..."
-pip install gdown
+pip install --no-cache-dir gdown
 
 echo "Downloading model file from Google Drive..."
-gdown https://drive.google.com/uc?id=1SGgdsz_eDKrdD4qw5YlQXQigwrS6frim
-
-echo "Moving model to predictor folder..."
-mv credit_score_model.pkl predictor/
+gdown https://drive.google.com/uc?id=1SGgdsz_eDKrdD4qw5YlQXQigwrS6frim -O predictor/credit_score_model.pkl
 
 echo "Running Django migrations..."
-python manage.py migrate
+python manage.py migrate --no-input
 
 echo "Starting Django app with Gunicorn..."
-gunicorn credit_predictor.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn credit_predictor.wsgi:application --bind 0.0.0.0:8000 --workers 2
